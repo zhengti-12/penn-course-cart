4b7b68b81c7beab4ecbde09cb1ce34e16ea2bb7e
Tiffany <tiffanyyouwu@gmail.com>
2026-01-29 12:33:02 -0500

    Initial commit

---
+++ src/components/Courses.tsx
@@ -0,0 +1,66 @@
+import courses from "../data/courses.json"
+import React, { useState } from "react"
+
+const Courses = () => {
+	const initialCartState = courses.map(() => false) // Initialize cart state for each course
+	const [cart, setCart] = useState(initialCartState)
+
+	const handleToggleCartClick = (courseIndex: number) => {
+		const updatedCart = [...cart]
+		updatedCart[courseIndex] = !updatedCart[courseIndex] // Toggle the state
+		setCart(updatedCart)
+		if (!updatedCart[courseIndex]) {
+			alert("Removed from Cart")
+		}
+	}
+
+	return (
+		<>
+			{courses.map(
+				(
+					{
+						dept,
+						number,
+						title,
+						description,
+						prereqs,
+						"cross-listed": crossListed,
+					},
+					index
+				) => (
+					<div key={`${dept}-${number}`}>
+						<br />
+						<b>
+							{dept} {number}: {title}
+						</b>
+						<br />
+						{description}
+						{prereqs && prereqs.length > 0 && (
+							<>
+								<br />
+								<i>
+									{" "}
+									Prerequisites:{" "}
+									{Array.isArray(prereqs) ? prereqs.join(", ") : prereqs}{" "}
+								</i>
+							</>
+						)}
+						{crossListed && crossListed.length > 0 && (
+							<>
+								<br />
+								Cross-listed: {crossListed.join(", ")}
+							</>
+						)}
+						<div>
+							<button onClick={() => handleToggleCartClick(index)}>
+								{cart[index] ? "Remove from Cart" : "Add to Cart"}
+							</button>
+						</div>
+					</div>
+				)
+			)}
+		</>
+	)
+}
+
+export default Courses
